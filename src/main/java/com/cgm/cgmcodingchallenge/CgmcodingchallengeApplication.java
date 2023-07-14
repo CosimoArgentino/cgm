4986843d8e679e58a8e5fff7113a447ba433c586
Cosimo Argentino <cosimoargentino@gmail.com>
2023-07-14 20:34:11 +0200

    first commit

---
+++ src/main/java/com/cgm/cgmcodingchallenge/CgmcodingchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.cgm.cgmcodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgmcodingchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgmcodingchallengeApplication.class, args);
+	}
+
+}
